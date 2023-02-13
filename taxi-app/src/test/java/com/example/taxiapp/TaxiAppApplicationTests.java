909b8510492464d848536e9994012b79802d095e
M <rebelsowl53@gmail.com>
2023-02-13 09:53:10 +0300

    taxi app init

---
+++ taxi-app/src/test/java/com/example/taxiapp/TaxiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
